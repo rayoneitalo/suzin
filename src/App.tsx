3aac9e826a141801584f2ed127a4d7e1bf6c944e
1kxk <caiow.wk@gmail.com>
2020-08-14 02:12:24 -0300

    feat: create route and login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
 
+import Routes from './routes';
+
 const App = () => {
   return (
     <>
       <GlobalStyles />
+      <Routes />
     </>
   );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Login from './pages/Login';
+
 const Routes: React.FC = () => {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' componet={} />
+          <Route exact path='/login' component={Login} />
         </Switch>
       </BrowserRouter>
     </>
