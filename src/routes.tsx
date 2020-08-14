f9593a97319307af3459a83243030f57fb573c99
1kxk <caiow.wk@gmail.com>
2020-08-14 02:08:23 -0300

    feat: routes file with react-router-dom

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import {} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Routes: React.FC = () => {
   return (
-
-  )
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' componet={} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 };
+
+export default Routes;
