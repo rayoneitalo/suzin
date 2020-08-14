95ce56a91d8bf800162717151745f0c4bf668a26
1kxk <caiow.wk@gmail.com>
2020-08-13 23:18:12 -0300

    root colors

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -2,6 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
 
+:root {
+  --color-primary: #3A2313;
+  --color-secondary: #FA9852;
+  --color-secondary-hover: #C77942;
+  /* --color-: #FBC59E;
+  --color-: #7A604D; */
+}
+
 * {
   margin: 0;
   padding: 0;
@@ -14,7 +22,6 @@ html, body, #root {
 
 *, input, button {
   border: 0;
-  background: none;
-  
+  background: none; 
 }
 `;
