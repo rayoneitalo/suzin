7dd3db728256a0a7f2b78e6f79e7b709d2001a0b
1kxk <caiow.wk@gmail.com>
2020-08-14 02:21:30 -0300

    feat(global-style): change the default font and make responsible to mobile devices

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -8,12 +8,19 @@ export default createGlobalStyle`
   --color-text: #695044;          /** p, span */
   --color-background: #F2EEEB;
   /* --color-background: #D6BA9D; */
+
+  font-size: 60%;
+
+  @media (min-width: 700px) {
+    font-size: 62.5%;
+  }
 }
 
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: Open sans Condensed, sans-serif;
 }
 
 html, body, #root {
