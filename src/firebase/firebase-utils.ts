8e9da9aa00156c06ae7a04c428d2c84f672dd934
1kxk <caiow.wk@gmail.com>
2020-08-15 00:11:13 -0300

    feat(firebase): user data stored in firestore

--- src/firebase/firebase-utils.ts
+++ src/firebase/firebase-utils.ts
@@ -15,13 +15,25 @@ const firebaseConfig = {
 
 export const createUserProfileDocument = async (
   userAuth: firebase.User | null,
-  additionalData: string
+  additionalData: any
 ) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
-  console.log(snapShot);
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(firebaseConfig);
